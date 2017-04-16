6f9aa7ea84a1f024d02f2397590a8de9d78bf856
Antonio Coppola <coppola.ant@gmail.com>
2017-04-16 01:33:00 +0000

    working API

---
+++ README.md
@@ -0,0 +1,17 @@
+# Timestamps Microservice exercise for FCC
+---
+
+Endpoints:
+
+`/:unix-timestamp` 
+
+`/:month day, year`
+return the date in format "Month DD, YYYY" and in Unix format
+
+Example:
+```json
+{
+"unix": "1452556800",
+"natural": "January 12, 2016"
+}
+
--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 var express = require("express")
 var app = express()
+var moment = require("moment")
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.use('/', express.static(__dirname + '/public'));
+
+app.get('/:date', function (req, res) {
+    var date;
+    if (/[a-z]+ ([0-9])+, \d{4}/g.test(req.params.date.toLowerCase())) {
+        date = moment(req.params.date, "MMMM D, YYYY");
+    } else if (/^\d+$/g.test(req.params.date)) {
+        date = moment(req.params.date, "X")
+    }
+    
+    if(date && date.isValid()) {
+        res.json({
+          unix: date.format("X"),
+          natural: date.format("MMMM D, YYYY")
+        });
+      } else {
+        res.json({
+          unix: null,
+          natural: null
+        });
+      }
 })
 
 app.listen(8080, function () {
