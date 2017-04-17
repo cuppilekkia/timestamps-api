4d362187ff6c7c8e039cff1ca8ca9f9394ebedd3
Antonio Coppola <coppola.ant@gmail.com>
2017-04-17 11:03:40 +0100

    Update server.js

--- server.js
+++ server.js
@@ -25,6 +25,6 @@ app.get('/:date', function (req, res) {
       }
 })
 
-app.listen(8080, function () {
+app.listen(process.env.PORT || 8080, function () {
   console.log('Example app listening on port 8080!')
 })
